8208df79d64a7af5b835fea1f6aaeb2ca3922418
karthikkraj <119588560+karthikkraj@users.noreply.github.com>
2025-03-17 03:53:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 3000;
+const hostname = 'localhost'; // Change this from '192.0.0.1' to 'localhost'
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Jenkins Pipeline!\n');
+  res.end('Hello, my name is Karthik Raj and my roll number is 2022BCD0041\n');
+});
+res.end('This is DevOps Assignment 2 with Jenkins and SonarQube Pipelining\n');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
