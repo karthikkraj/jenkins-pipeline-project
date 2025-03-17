c482d92e1bb9ab2f7a0f080932510d8c29955a9a
karthikkraj <119588560+karthikkraj@users.noreply.github.com>
2025-03-17 11:12:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-// app.js
 const http = require('http');
-
 const hostname = '0.0.0.0';
 const port = 9090;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, I am Karthik Raj, and my roll number is 2022BCD0041\n' + 'I have completed the DevOps pipeline assignment, which includes integrating Git, SonarQube, Docker, and deployment.');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Hello from Node.js</title>
+      </head>
+      <body>
+        <h3>Hello!</h3>
+        <p>I am Karthik Raj, and my roll number is 2022BCD0041</p>
+        <p>I have completed the DevOps pipeline assignment, which includes integrating Git, SonarQube, Docker, and deployment.</p>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(port, hostname, () => {
